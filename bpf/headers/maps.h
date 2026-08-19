b1ef067737152cd65e4971e83965fe87b45c73a9
Aakarsh Maurya <mauryaaakarsh11@gmail.com>
2026-08-20 02:02:56 +0530

    chore(xdp): Added header files
    Signed-off-by: Aakarsh Maurya <mauryaaakarsh11@gmail.com>

---
+++ bpf/headers/maps.h
@@ -0,0 +1,42 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+#define MAX_ENTRIES 100000
+#define ETH_P_IPV6 0x86DD
+#define ETH_P_IP 0x0800 
+
+struct ipv4_lpm_key{
+    __u32 prefixlen;
+    __u32 addr;
+};
+
+struct ipv6_lpm_key{
+    __u32 prefixlen;
+    __u32 addr[4];
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_LPM_TRIE);
+    __type(key, struct ipv4_lpm_key);
+    __type(value, __u32);
+    __uint(map_flags, BPF_F_NO_PREALLOC);
+    __uint(max_entries, MAX_ENTRIES);
+} ipv4_lpm_map SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_LPM_TRIE);
+    __type(key, struct ipv6_lpm_key);
+    __type(value, __u32);
+    __uint(map_flags, BPF_F_NO_PREALLOC);
+    __uint(max_entries, MAX_ENTRIES);
+} ipv6_lpm_map SEC(".maps");
+
+
+struct hdr_cursor{
+    void *pos;
+};
+
+#endif
